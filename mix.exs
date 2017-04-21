812f98e465f2c8d4e5495ce85f0d87b3072f6c3c
Michał Kalbarczyk <fazibear@gmail.com>
2017-04-21 19:26:12 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Export.Mixfile do
       app: :export,
       description: "Erlport wrapper for Elixir",
       package: package(),
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
