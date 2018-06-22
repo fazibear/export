3bfabf67b8284a22370c7859461e36e6e09d89ba
Jeffery Utter <jeff@jeffutter.com>
2018-06-22 11:28:07 -0500

    Use erlport branch with otp-21 support

--- lib/export/helpers.ex
+++ lib/export/helpers.ex
@@ -6,10 +6,10 @@ defmodule Export.Helpers do
       case opt do
         {key, other_options} when other_options |> is_list ->
           {key, other_options |> Enum.map(fn ({key, value}) ->
-            {key |> Atom.to_char_list, value |> String.to_char_list}
+            {key |> Atom.to_charlist, value |> String.to_charlist}
           end)}
         {key, value} when value |> is_bitstring ->
-          {key, value |> String.to_char_list}
+          {key, value |> String.to_charlist}
         _ -> opt
       end
     end)
